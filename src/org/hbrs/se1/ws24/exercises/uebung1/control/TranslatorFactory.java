d299981fbed18345fe8fff1f0815cc5b0ab6ffe3
IWCBN <peter.droste@smail.inf.h-brs.de>
2024-10-04 17:05:50 +0200

    Test hinzugefügt und Fehler in Übersetzung 5 korrigiert.

--- src/org/hbrs/se1/ws24/exercises/uebung1/control/TranslatorFactory.java
+++ src/org/hbrs/se1/ws24/exercises/uebung1/control/TranslatorFactory.java
@@ -3,15 +3,12 @@ package org.hbrs.se1.ws24.exercises.uebung1.control;
 public class TranslatorFactory {
     //private static GermanTranslator translator1;
 
-    public static Translator createTranslator(){
+    public static Translator creatTranslator(){
+        /*if(translator1==null){
+            GermanTranslator translator1 = new GermanTranslator();
+        }*/
 
         return new GermanTranslator();
-    }
-
-    public static Translator createTranslator(String date){
-        GermanTranslator g1 = new GermanTranslator();
-        g1.setDate(date);
-        return g1;
     }
 
 }
