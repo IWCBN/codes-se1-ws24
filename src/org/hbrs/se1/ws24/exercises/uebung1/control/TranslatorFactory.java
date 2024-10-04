e438f34f2fa12599e723e17df4734ef438d906eb
ki-fer <adrian.kiefer@smail.inf.h-brs.de>
2024-10-04 17:01:29 +0200

    Alle Dateien aus dem alten Projekt hinzugefügt.

--- src/org/hbrs/se1/ws24/exercises/uebung1/control/TranslatorFactory.java
+++ src/org/hbrs/se1/ws24/exercises/uebung1/control/TranslatorFactory.java
@@ -3,12 +3,12 @@ package org.hbrs.se1.ws24.exercises.uebung1.control;
 public class TranslatorFactory {
     //private static GermanTranslator translator1;
 
-    public static Translator creatTranslator(){
+    public static Translator createTranslator(){
 
         return new GermanTranslator();
     }
 
-    public static Translator creatTranslator(String date){
+    public static Translator createTranslator(String date){
         GermanTranslator g1 = new GermanTranslator();
         g1.setDate(date);
         return g1;
