dac98934275f8d30ffcece3994a46ba966a82fd4
IWCBN <peter.droste@smail.inf.h-brs.de>
2024-10-10 14:30:48 +0200

    ConcreteMember.java beispiel Implementierung mit dem Member.java Interface erstellt.

---
+++ src/org/hbrs/se1/ws24/exercises/uebung2/ConcreteMember.java
@@ -0,0 +1,47 @@
+package org.hbrs.se1.ws24.exercises.uebung2;
+
+/**
+ * Diese Klasse dient als beispiel Implementierung eines Members.
+ */
+public class ConcreteMember implements Member{
+
+  private final Integer ID;
+
+  /**
+   * Konstruktor, welcher ein Member-Objekt erzeugt mit vorgegebener ID.
+   *
+   * @param id ist die ID dieses Member-Objekts
+   */
+  public ConcreteMember(Integer id){
+    this.ID =id;
+  }
+
+  /**
+   * Die ID ist über einen Konstruktor einer Klasse, welches dieses Interface implementiert,
+   * zu setzen. Die ID darf nicht innerhalb des Container-Objekts gesetzt werden.
+   * Die ID dient als Primärschlüssel zur Unterscheidung aller Member-Objekte.
+   */
+  @Override
+  public Integer getID() {
+    return ID;
+  }
+
+
+  /**
+   * Prüft, ob dieses Member-Objekt(<code>this</code>) mit dem übergebenen Objekt(<code>obj</code>) identisch ist
+   *
+   * @param obj das zu vergleichende Objekt
+   * @return true, wenn die Objekte gleich sind, sonst false
+   */
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj){
+      return true;
+    }
+    if(obj instanceof Member) {
+      return this.getID().equals(((Member) obj).getID());
+    }
+    return false;
+  }
+
+}
