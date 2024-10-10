fec4b9e4b9025fa0232fc36c8163944278f2c02f
ki-fer <adrian.kiefer@smail.inf.h-brs.de>
2024-10-10 14:17:54 +0200

    Create Methode mit Datum hinzugefügt.

--- src/org/hbrs/se1/ws24/exercises/uebung2/ContainerException.java
+++ src/org/hbrs/se1/ws24/exercises/uebung2/ContainerException.java
@@ -2,7 +2,7 @@ package org.hbrs.se1.ws24.exercises.uebung2;
 
 public class ContainerException extends IllegalArgumentException {
     public ContainerException(int id) {
-        super();
+        super();//Die Fehlernachricht muss noch hinzugefügt werden.
     }
 
 }
