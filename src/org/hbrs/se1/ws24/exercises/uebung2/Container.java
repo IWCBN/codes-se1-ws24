8bee175b323df3638091bae8cbb8513a8d0fdb7f
ki-fer <adrian.kiefer@smail.inf.h-brs.de>
2024-10-10 14:27:40 +0200

    deleteMember Methode hinzugefügt.

--- src/org/hbrs/se1/ws24/exercises/uebung2/Container.java
+++ src/org/hbrs/se1/ws24/exercises/uebung2/Container.java
@@ -8,9 +8,21 @@ public class Container {
 
         for(Member person : members){
             if(person.getID() == member.getID()){
-                throw new ContainerException();
+                throw new ContainerException(member.getID());
             }
         }
         members.add(member);
     }
+
+    public String deleteMember(int id) {
+        for(Member member : members){
+            if(member.getID() == id){
+                members.remove(member);
+                return "Folgender Member wurde gelöscht: " + member.getID();
+            }
+        }
+        return "Es existiert kein Member mit Folgender Member ID: " + id;
+    }
+
+
 }
