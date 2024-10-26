2a48439b33491fc6c4b5a3c057e6ccb2c13b5540
ki-fer <adrian.kiefer@smail.inf.h-brs.de>
2024-10-26 15:56:55 +0200

    UserStory Klasse Methoden hinzugefügt.

--- src/org/hbrs/se1/ws24/exercises/uebung4/UserStory.java
+++ src/org/hbrs/se1/ws24/exercises/uebung4/UserStory.java
@@ -1,18 +1,37 @@
 package org.hbrs.se1.ws24.exercises.uebung4;
 
 public class UserStory implements UserStoryInterface {
-    private int id;
+    private final int id;
     private String title;
+    private String acceptanceCriterion;
+    private int prioritization;
+    private String project;
+    private int businessValue;
+    private int effort;
+    private int risk;
+    private int penalty;
+    private static int nextId = 1;
 
+    public UserStory(String title, String acceptanceCriterion, String project, int businessValue, int effort, int risk, int penalty) {
+        id = nextId++;
+        this.title = title;
+        this.acceptanceCriterion = acceptanceCriterion;
+        this.project = project;
+        this.businessValue = businessValue;
+        this.effort = effort;
+        this.risk = risk;
+        this.penalty = penalty;
+        calculatePrioritization();
+    }
 
-    public UserStory() {
-
+    private void calculatePrioritization() {
+        prioritization = (businessValue+penalty)/(effort+risk);
     }
 
 
     @Override
     public int getID() {
-        return 0;
+        return id;
     }
 
 
@@ -23,39 +42,42 @@ public class UserStory implements UserStoryInterface {
 
     @Override
     public void setTitle(String title) {
-
+        this.title = title;
     }
 
     @Override
     public String getAcceptanceCriterion() {
-        return "";
+        return acceptanceCriterion;
     }
 
     @Override
     public void setAcceptanceCriterion(String acceptanceCriterion) {
-
+        this.acceptanceCriterion = acceptanceCriterion;
     }
 
     @Override
     public int getPrioritization() {
-        return 0;
+        return prioritization;
     }
 
     @Override
     public String getProject() {
-        return "";
+        return this.project;
     }
 
     @Override
     public void setProject(String project) {
-
+        this.project = project;
     }
 
-    public boolean equals(UserStory userStory) {
+    public boolean equals(Object userStory) {
+        if(userStory instanceof UserStory) {
+            return id == ((UserStory)userStory).getID();
+        }
         return false;
     }
 
     public String toString() {
-        return "";
+        return "ID: "+ id +"Titel: " + title;
     }
 }
