f200184391d0c10277ec9ce69ab32ac30cad98cf
IWCBN <peter.droste@smail.inf.h-brs.de>
2024-10-30 20:19:07 +0100

    ContainerException.java bekam 2 weitere Konstruktoren und überarbeitung der Javadocs.

--- src/org/hbrs/se1/ws24/exercises/uebung2/ContainerException.java
+++ src/org/hbrs/se1/ws24/exercises/uebung2/ContainerException.java
@@ -3,12 +3,40 @@ package org.hbrs.se1.ws24.exercises.uebung2;
 public class ContainerException extends IllegalArgumentException {
 
     /**
-     * Konstruktor, welcher ein Objekt vom Typ IllegalArgumentException erzeugt und eine individuelle Fehlernachricht übergibt.
-     *
+     * Konstruktor, welcher ein Objekt vom Typ IllegalArgumentException erzeugt und eine individuelle
+     * Fehlernachricht übergibt. Hier wird eine Fehlernachricht für die Member ID angegeben.
+     * <p>
+     * Beispiel:
+     * <code>Das Member-Objekt mit der ID 1 ist bereits vorhanden!</code>
+     *</p>
      * @param id ist die Member ID, die in der Fehlernachricht angezeigt werden soll.
      */
     public ContainerException(int id) {
         super("Das Member-Objekt mit der ID " + id + " ist bereits vorhanden!");
+    }
+
+    /**
+     * Konstruktor, welcher ein Objekt vom Typ IllegalArgumentException erzeugt und die möglichkeit gibt
+     * eine Fehlermeldung anzugeben.
+     *
+     * @param message ist die Fehlernachricht
+     */
+    public ContainerException(String message) {
+        super(message);
+    }
+
+    /**
+     * Konstruktor, welcher ein Objekt vom Typ IllegalArgumentException erzeugt und eine
+     * individuelle Fehlernachricht übergeben. Hier wird eine Fehlernachricht für ein gegebenes Objekt angegeben.
+     * Zum Generieren des Textes wird die toString()-Methode der Klasse Object genutzt.
+     * <p>
+     * Beispiel:
+     * <code>Das Objekt: o.toString() ist bereits vorhanden!</code>
+     * </p>
+     * @param o
+     */
+    public ContainerException(Object o) {
+        super("Das Objekt: " + o + " ist bereits vorhanden!");
     }
 
 }
