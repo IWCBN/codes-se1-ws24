b10a20560c61f4d4471f505cde42f95a29233df2
ki-fer <adrian.kiefer@smail.inf.h-brs.de>
2024-11-01 18:46:34 +0100

    Help Menü in separate Methode auslagern.

--- src/org/hbrs/se1/ws24/exercises/uebung4/CommandHandler.java
+++ src/org/hbrs/se1/ws24/exercises/uebung4/CommandHandler.java
@@ -18,19 +18,7 @@ public class CommandHandler {
           System.exit(0);
           break;
         case "help":
-          System.out.println("Folgende Befehle stehen zur Verfügung:\nexit: Beendet das Programm.\nhelp: Gibt genau diese Liste aus.");
-          System.out.println("enter [\"Titel\" \"Akzeptanzkriterium\" \"Mehrwert\" \"Strafe\" \"Aufwand\" \"Risiko\" \"Projekt Name\"]:");
-          System.out.println("\tLegt eine neue User Story in dem System an. Folgende Werte werden benötigt:");
-          System.out.println("\t\tTitel: Titel der User Story");
-          System.out.println("\t\tAkzeptanzkriterium: Beschreibt Testfälle mit denen das erfolgreiche Abschließen einer User Story erreicht werden kann.");
-          System.out.println("\t\tMehrwert: Ist eine Zahl, die den Mehrwert für das Unternehmen beziffert.");
-          System.out.println("\t\tStrafe: Ist eine Zahl, die die Höhe der Strafe beziffert.");
-          System.out.println("\t\tAufwand: Ist eine Zahl, die den Aufwand beziffert um die User Story umzusetzen.");
-          System.out.println("\t\tRisiko: Ist eine Zahl, die das Risiko beziffert wenn die User Story nicht umgesetzt wird.");
-          System.out.println("\t\tProjekt Name: Gibt den Namen des zugehörigen Projektes aus.");
-          System.out.println("store: Speichert den aktellen Stand der User Stories");
-          System.out.println("load: Lädt den aktuellen Stand der User Stories von der Festplatte");
-
+          printHelp();
           break;
         case "save":
           try {
@@ -59,5 +47,20 @@ public class CommandHandler {
           break;
       }
     }
+  }
+
+  private static void printHelp(){
+    System.out.println("\nFolgende Befehle stehen zur Verfügung:\nexit: Beendet das Programm.\nhelp: Gibt genau diese Liste aus.");
+    System.out.println("enter [\"Titel\" \"Akzeptanzkriterium\" \"Mehrwert\" \"Strafe\" \"Aufwand\" \"Risiko\" \"Projekt Name\"]:");
+    System.out.println("\tLegt eine neue User Story in dem System an. Folgende Werte werden benötigt:");
+    System.out.println("\t\tTitel: Titel der User Story");
+    System.out.println("\t\tAkzeptanzkriterium: Beschreibt Testfälle mit denen das erfolgreiche Abschließen einer User Story erreicht werden kann.");
+    System.out.println("\t\tMehrwert: Ist eine Zahl, die den Mehrwert für das Unternehmen beziffert.");
+    System.out.println("\t\tStrafe: Ist eine Zahl, die die Höhe der Strafe beziffert.");
+    System.out.println("\t\tAufwand: Ist eine Zahl, die den Aufwand beziffert um die User Story umzusetzen.");
+    System.out.println("\t\tRisiko: Ist eine Zahl, die das Risiko beziffert wenn die User Story nicht umgesetzt wird.");
+    System.out.println("\t\tProjekt Name: Gibt den Namen des zugehörigen Projektes aus.");
+    System.out.println("store: Speichert den aktellen Stand der User Stories");
+    System.out.println("load: Lädt den aktuellen Stand der User Stories von der Festplatte\n");
   }
 }
