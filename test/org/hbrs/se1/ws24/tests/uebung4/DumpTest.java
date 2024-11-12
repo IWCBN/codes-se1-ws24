43147d7e2682392405fb5375a8932d1a5e473f6a
ki-fer <adrian.kiefer@smail.inf.h-brs.de>
2024-11-12 23:13:44 +0100

    Test Dump

--- test/org/hbrs/se1/ws24/tests/uebung4/DumpTest.java
+++ test/org/hbrs/se1/ws24/tests/uebung4/DumpTest.java
@@ -4,6 +4,7 @@ import org.hbrs.se1.ws24.exercises.uebung4.Container;
 import org.hbrs.se1.ws24.exercises.uebung4.UserStory;
 import org.hbrs.se1.ws24.exercises.uebung4.UserStoryInterface;
 import org.hbrs.se1.ws24.exercises.uebung4.view.ContainerView;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -27,10 +28,11 @@ public class DumpTest {
     public void setUp() {
         out = new ByteArrayOutputStream();
         System.setOut(new PrintStream(out));
+        UserStory.setNextId(0);
         list = new LinkedList<>();
         story = new UserStory("test", "test", "test", 1, 1, 1, 1);
         story2 = new UserStory("Hallo wie geht es dir heute? Wird das alles Klappen?", "test", "test", 1, 1, 1, 1);
-        story3 = new UserStory("Hallo wie geht es dir heute? Wird das alles Klappen?", "test", "test", 1, 1, 454545454, 1);
+        story3 = new UserStory("Hallo wie geht es dir heute? Wird das alles Klappen?", "test", "test2", 1, 1, 454545454, 1);
     }
 
     @Test
@@ -44,18 +46,61 @@ public class DumpTest {
     public void dumpNormalListTest(){
         list.addFirst(story);
         ContainerView.dump(list,null);
-        String soll = "| ID   | Titel                | Kriterium            | Projekt              | BusinessValue | Effort | Risk | Penalty | Prioritization |\r\n| 1    | test                 | test                 | test                 | 1.0           | 1.0    | 1.0  | 1.0     | 1.0            |\r\n";
-        //assertEquals(soll, out.toString());
-        System.setOut(originalOut);
-        System.out.println(out.toString());
-
-        System.out.println(soll);
-
+        String soll = "| ID   | Titel                | Kriterium            | Projekt              | BusinessValue | Effort | Risk | Penalty | Prioritization |\n" +
+                "| 1    | test                 | test                 | test                 | 1.0           | 1.0    | 1.0  | 1.0     | 1.0            |\n";
         assertEquals(soll, out.toString());
     }
+
+    @Test
+    public void dumpLongListTest(){
+        list.addFirst(story3);
+        list.addFirst(story2);
+        list.addFirst(story);
+        ContainerView.dump(list,null);
+
+        String soll = "| ID   | Titel                | Kriterium            | Projekt              | BusinessValue | Effort | Risk | Penalty | Prioritization |\n" +
+                "| 1    | test                 | test                 | test                 | 1.0           | 1.0    | 1.0  | 1.0     | 1.0            |\n" +
+                "| 2    | Hallo wie geht es di | test                 | test                 | 1.0           | 1.0    | 1.0  | 1.0     | 1.0            |\n" +
+                "|      | r heute? Wird das al |                      |                      |               |        |      |         |                |\n" +
+                "|      | les Klappen?         |                      |                      |               |        |      |         |                |\n" +
+                "| 3    | Hallo wie geht es di | test                 | test2                | 1.0           | 1.0    | 4.54 | 1.0     | 4.3999999956E- |\n" +
+                "|      | r heute? Wird das al |                      |                      |               |        | 5454 |         | 9              |\n" +
+                "|      | les Klappen?         |                      |                      |               |        | 54E8 |         |                |\n" +
+                "|      |                      |                      |                      |               |        |      |         |                |\n";
+        assertEquals(soll, out.toString());
+    }
+
+    @Test
+    public void dumpFilterListTest(){
+        list.addFirst(story3);
+        list.addFirst(story2);
+        list.addFirst(story);
+        ContainerView.dump(list,"test");
+        String soll = "| ID   | Titel                | Kriterium            | Projekt              | BusinessValue | Effort | Risk | Penalty | Prioritization |\n" +
+                "| 1    | test                 | test                 | test                 | 1.0           | 1.0    | 1.0  | 1.0     | 1.0            |\n" +
+                "| 2    | Hallo wie geht es di | test                 | test                 | 1.0           | 1.0    | 1.0  | 1.0     | 1.0            |\n" +
+                "|      | r heute? Wird das al |                      |                      |               |        |      |         |                |\n" +
+                "|      | les Klappen?         |                      |                      |               |        |      |         |                |\n";
+        assertEquals(soll, out.toString());
+    }
+
+    @Test
+    public void dumpIntListTest(){
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        ContainerView.dump(list);
+        String soll = "1\n2\n3\n";
+        assertEquals(soll, out.toString());
+    }
+
+
+
 
     @AfterEach
     public void tearDown() {
         System.setOut(originalOut);
+
     }
 }
