f9254f0779203b79157f102fa6ca6efffb2fadb9
blueImoon <kblueka1001@gmail.com>
2025-01-08 11:17:39 +0100

    .

---
+++ test/org/hbrs/se1/ws24/tests/uebung10/xyz.java
@@ -0,0 +1,13 @@
+package org.hbrs.se1.ws24.tests.uebung10;
+
+import org.hbrs.se1.ws24.exercises.uebung4.UserStory;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class xyz {
+
+
+}
